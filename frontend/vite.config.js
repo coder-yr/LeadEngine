ff5cfc47f9068237edf5f95cacf01dc73e5deef4
Ramyash8 <yashram7160@csmu.ac.in>
2026-08-05 17:08:19 +0530

    deploy fix bakend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
     },
     server: {
         port: 5173,
-        open: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
